010ce25b097d0f9a13db8d46591e3fb7feff7ca2
xavier villa <xaviervilla@github.com>
2018-03-01 08:40:46 -0800

    Updated window size

--- app_main.cpp
+++ app_main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
     
     // Instantiate a new App
-    App* myApp = new App("MyApp", 50, 50, 600, 600);
+    App* myApp = new App("Tic-Tac-Toe", 50, 50, 1800, 1800);
 
 	// Start the app
     myApp->run();
